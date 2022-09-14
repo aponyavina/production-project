9eac22b8ac91cf3b7a52f11fdddbbdc1fe269381
aponyavina <ponyavina1997@mail.ru>
2022-09-14 16:33:18 +0300

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from "./Counter.module.scss"
+
+// именованный экспорт
+export const Counter = () => {
+    return (
+        <div className={styles.button}>
+            ff
+        </div>
+    );
+};
