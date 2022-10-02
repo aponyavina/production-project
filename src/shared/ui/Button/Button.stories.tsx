c262d25a318fc434180d82c42eb7bb11b0f1ee79
aponyavina <ponyavina1997@mail.ru>
2022-10-02 16:30:39 +0300

    настройка storybook, декораторов + стори кейсы на компоненты

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Button, ThemeButton } from './Button';
+
+export default {
+    title: 'shared/Button',
+    component: Button,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Text',
+};
+
+// темная тема
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    children: 'Text',
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const Clear = Template.bind({});
+Clear.args = {
+    children: 'Text',
+    theme: ThemeButton.CLEAR,
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+    children: 'Text',
+    theme: ThemeButton.OUTLINE,
+};
