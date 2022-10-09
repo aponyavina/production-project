b200932304e628cb6a13907bd6c51e2067ee07d4
aponyavina <ponyavina1997@mail.ru>
2022-10-09 18:37:26 +0300

    upgrade sidebar, add button themes

--- src/widgets/PageError/PageError.tsx
+++ src/widgets/PageError/PageError.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './PageError.module.scss';
 
 interface PageErrorProps {
@@ -18,7 +18,7 @@ const PageError = ({ className }: PageErrorProps) => {
     return (
         <div className={classNames(cls.PageError, {}, [className])}>
             <p>{t('Произошла непредвиденная ошибка')}</p>
-            <Button onClick={reloadPage} theme={ThemeButton.CLEAR}>
+            <Button onClick={reloadPage} theme={ButtonTheme.CLEAR}>
                 {t('Обновить страницу')}
             </Button>
         </div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import LangSwitcher from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import { useTranslation } from 'react-i18next';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import AboutIcon from 'shared/assets/icons/about-icon.svg';
+import MainIcon from 'shared/assets/icons/main-icon.svg';
+
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -24,14 +30,39 @@ const Sidebar = ({ className }: SidebarProps) => {
         >
             <Button
                 data-testid="sidebar-toggle"
-                theme={ThemeButton.CLEAR}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
                 onClick={onToggle}
+                className={cls.collapseBtn}
+                square
+                size={ButtonSize.L}
             >
-                {t('Переключить')}
+                {collapsed ? '>' : '<'}
             </Button>
+            <div className={cls.items}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                    className={cls.item}
+                >
+                    <MainIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('Главная')}
+                    </span>
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                    className={cls.item}
+                >
+                    <AboutIcon className={cls.icon} />
+                    <span className={cls.link}>
+                        {t('О сайте')}
+                    </span>
+                </AppLink>
+            </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <LangSwitcher short={collapsed} className={cls.lang} />
             </div>
         </div>
     );
