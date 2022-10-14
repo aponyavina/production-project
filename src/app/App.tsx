5fb5a145e03c27d113e65ee670cac79097337842
aponyavina <ponyavina1997@mail.ru>
2022-10-14 23:55:48 +0300

    fix theme for modal window

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,13 @@
 import React, { Suspense } from 'react';
 
 import { AppRouter } from 'app/providers/router';
-import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
-    const { theme } = useTheme();
     return (
-        <div className={classNames('app', { hovered: true, selected: false }, [theme, ''])}>
+        <div className={classNames('app', { hovered: true, selected: false }, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
