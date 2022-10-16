73f797f7462d6c20f6d584dfc555d59bc573866d
aponyavina <ponyavina1997@mail.ru>
2022-10-16 14:07:41 +0300

    add stories for input and login form

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -8,7 +8,7 @@ interface LoginFormProps {
     className?: string
 }
 
-const LoginForm = ({ className }: LoginFormProps) => {
+export const LoginForm = ({ className }: LoginFormProps) => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.LoginForm, {}, [className])}>
@@ -29,5 +29,3 @@ const LoginForm = ({ className }: LoginFormProps) => {
         </div>
     );
 };
-
-export default LoginForm;
