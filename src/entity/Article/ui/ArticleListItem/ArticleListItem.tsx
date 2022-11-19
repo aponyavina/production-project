ab0bf96017b990c0b6d2b70fd1862d2d1d26e18d
aponyavina <ponyavina1997@mail.ru>
2022-11-19 18:47:32 +0300

    add fetching articles, view type selector

--- src/entity/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entity/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { memo, useCallback } from 'react';
 import { Text } from 'shared/ui/Text/Text';
 import { Icon } from 'shared/ui/Icon/Icon';
-import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import EyeIcon from 'shared/assets/icons/eye-icon.svg';
 import { Card } from 'shared/ui/Card/Card';
 import { Avatar } from 'shared/ui/Avatar/Avatar';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
