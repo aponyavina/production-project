3b61c203f37baa6aee4e545468f1fe96c6906fcf
aponyavina <ponyavina1997@mail.ru>
2022-12-01 20:15:25 +0300

    production

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticleEditPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // не для реальных проектов!
-    setTimeout(() => resolve(import('./ArticleEditPage')), 400);
-}));
+export const ArticleEditPageAsync = lazy(() => import('./ArticleEditPage'));
